3e7548d41dc09b2916310c00f116037ba93c29f2
Abdella <abdella.shukur2@gmail.com>
2022-08-19 14:49:53 -0400

    all files working

---
+++ my-angular-build-2/src/app/contacts/contacts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+export class Contacts implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
